e42f6326e5f00e401fe84235ca3fc5e69f6e7f12
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2025-05-24 20:36:42 +0100

    fix error thrown when export canceled
    previously an error was thrown by opening the save file dialog for export and then closing it, this is no longer the case

--- src/window.py
+++ src/window.py
@@ -116,10 +116,14 @@ class InspectorWindow(Adw.ApplicationWindow):
 
     def file_save_response(self, dialog, result):
         global report
-        file = dialog.save_text_file_finish(result)
-        report_bytes = GLib.Bytes.new(report.encode("utf-8"))
-        file[0].replace_contents_bytes_async(report_bytes, None, False, Gio.FileCreateFlags.NONE)
-        del report
+        try:
+            file = dialog.save_text_file_finish(result)
+        except:
+            pass
+        else:
+            report_bytes = GLib.Bytes.new(report.encode("utf-8"))
+            file[0].replace_contents_bytes_async(report_bytes, None, False, Gio.FileCreateFlags.NONE)
+            del report
 
     def execute_terminal_command(self, command):
         if 'FLATPAK_ID' in os.environ:
